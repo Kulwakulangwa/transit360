0a94be1bc22606bccb0756721e0ab5e026ada952
Kulwakulangwa <kulwakulangwa@gmail.com>
2026-06-13 16:16:16 +0300

    Update shipments.tsx

--- src/routes/_authenticated/shipments.tsx
+++ src/routes/_authenticated/shipments.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router';
-import ShipmentsPage from '../pages/Shipments';   // <-- change from 'ShipmentsPage' to 'Shipments'
+import ShipmentsPage from '../pages/Shipments';
 
 export const Route = createFileRoute('/_authenticated/shipments')({
   component: ShipmentsPage,
